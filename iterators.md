0d1c0dec8cda62cd56c7cd5f3cb55cec1f9402cf
Paris Jones <bayarette@gmail.com>
2015-02-11 10:48:34 -0800

    Ruby Iterators Assignment with Testing

--- iterators.md
+++ iterators.md
@@ -6,40 +6,40 @@ Below you will find a list of methods. In the space provided below each, please 
 ###Array methods:
 May be helpful to look in [Enumerable](http://ruby-doc.org/core-2.2.0/Enumerable.html) as well...
 
-####select:
+####select: will return an array of the items specified in the block; otherwise, if no block present, will return an enumerator.
 
-####reject:
+####reject: will return an array of the items not specified to be excluded from the resulting array in the block; otherwise, if no block present, will return an enumerator
 
-####map:
+####map: will return another array after the work specified in the block has been performed on the original array; otherwise, if no block present, will return an enumerator 
 
-####detect:
+####detect/find: will return the first item from an array specified by the block, or will return nil if nothing matches.  
 
-####inject:
+####inject: will combine the values of an array in the manner specified by the block by iterating through the array until all items are combined. It will return the final value. Default initial value is the value of the first item in the array, otherwise can specify. 
 
-####partition:
+####partition: will return a two-dimensional array containing one array in which the elements were considered to be 'true' by the block, and another array where the elements that weren't considered 'true' were grouped.  
 
-####sort:
+####sort: will return an array of strings or integers sorted by the designations of the block. 
 
-####one:
+####one: will return the boolean true if there is only one item in the array that meets the block's specifications. 
 
-####none:
+####none?: will return a boolean of whether an array contains the element specified in the block or not. It will return 'true' if none of them meet the criteria, false otherwise.
 
-####all:
+####all?: returns a boolean: true if none of the elements return a value of nil or false, false otherwise.
 
-####empty?:
+####empty?: returns a boolean: true if the array is empty; false otherwise. 
 
-####eql?:
+####eql?: returns boolean: true if identical array is compared to the original; false otherwise.
 
-####include?:
+####include?: returns boolean: true if the item specified is in the array, false if it is not. 
 
-####nil?:
+####nil?: returns boolean: true if the item is nil (nil.nil?) and false otherwise ("".nil? == false)
 
 ###Hash methods:
 
-####key?:
+####key?: returns boolean: true if the hash contains the key specified in the block; false if it doesn't. 
 
-####keys:
+####keys: returns an array of symbols/keys if they are present in the hash as specified by the block. 
 
-####delete:
+####delete: returns the value of the deleted key.
 
-####delete_if:
+####delete_if: checks if certain values are present in a hash and deletes the key/value pair if it is present, and returns the remainder
--- spec/iterators_spec.rb
+++ spec/iterators_spec.rb
@@ -18,98 +18,108 @@ end
   # reject
   describe "#reject" do
     it "should reject even numbers" do
-        expect().to eq()
+        expect(@arr.reject { |num| num.even?}).to eq([1,3,5])
       end
   end
 
   # collect / map
   describe "#map" do
     it "should square each number" do
-        expect().to eq()
+        expect(@arr.map { |num| num*num}).to eq([1,4,9,16,25])
       end
   end
 
   # detect / find
   describe "#detect" do
     it "should detect all numbers divisible by 2 and 4" do
-        expect().to eq()
+        expect(@arr.detect{ |num| num % 2 == 0 and num % 4 == 0}).to eq(4)
       end
   end
 
   # inject
   describe "#inject" do
     it "should sum up all the numbers" do
-      expect().to eq()
+      expect(@arr.inject{ |num, num2| num + num2}).to eq(15)
     end
   end
 
   # partition
   describe "#partition" do
     it "should return an array of partitioned arrays " do
-      expect().to eq()
+      expect(@arr.partition{|num| num.even?}).to eq([[2, 4], [1, 3, 5]])
     end
   end
 
   # sort
   describe "#sort" do
     it "should sort an array of numbers" do
-      expect().to eq()
+      expect(@arr.sort{ |num1, num2| num2 <=> num1}).to eq([5, 4, 3, 2, 1])
     end
   end
+
+
 
   # one?
   describe "#one" do
     it "should return true if only one element meets the condition" do
-      expect().to be()
+      expect(@arr.one?{ |num| num == 2}).to be(true)
     end
     it "should return false if more than one element meets the condition" do
-      expect().to be()
+      expect(@arr.one?{ |num| num % 2 == 0}).to be(false)
     end
   end
 
-  # none?
+
+
+
+
+  ### none?
   describe "#none" do
     it "should return true if the number is not in the array " do
-      expect().to be()
+      expect(@arr.none? { |num| num <= 0}).to be(true)
     end
     it "should return false if the number is in the array " do
-      expect().to be()
+      expect(@arr.none? { |num| num == 2 }).to be(false)
     end
   end
 
   #all?
-  describe "#all" do
+  describe "#all?" do
     it "should return true if the condition passes at all elements in the array" do
-      expect().to be()
+      expect(@arr.all? { |num| num >= 0}).to be(true)
     end
   end
+
+
 
   #empty?
   describe "#empty?" do
     it "should return false if the array is not empty" do
-      expect().to be()
+      expect(@arr.empty?).to be(false)
     end
   end
+
 
   #eql?
   describe "#eql?" do
     it "should return true if an identical array is compared" do
-      expect().to be()
+      expect(@arr.eql?([1, 2, 3, 4, 5])).to be(true)
     end
   end
+
 
   #include?
   describe "#include?" do
     it "should return true if the parameter passed to include? is in the array" do
-      expect().to be(true)
-      expect().to be(false)
+      expect(@arr.include?(2)).to be(true)
+      expect(@arr.include?(18)).to be(false)
     end
   end
 
   #nil?
   describe "#nil?" do
     it "should return true if the object is nil" do
-      expect().to be()
+      expect(@nil.nil?).to be(true)
     end
   end
 
@@ -118,29 +128,30 @@ end
   #key?
   describe "hash method - #key?" do
     it "should return true if hash contains a key of :b" do
-      expect().to be()
+      expect(@hash.key?(:b)).to be(true)
     end
   end
 
   #keys
   describe "hash method - #keys" do
     it "should return array of symbols" do
-      expect().to eq()
+      expect(@hash.keys{ |num|   }).to eq([:a, :b])
     end
   end
 
   #delete
   describe "hash method - #delete" do
     it "should return value of key that is deleted" do
-      expect().to eq()
+      expect(@hash.delete(:a)).to eq(100)
     end
   end
 
   #delete_if
   describe "hash method - #delete_if" do
     it "should delete key/value pair for any value equal to 100" do
-      expect().to eq()
+      expect(@hash.delete_if { |key, value| value == 100}).to eq(:b => 200)
     end
   end
+
 
 end #end describe iterators
